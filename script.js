b69f8a147e90fc93dc6a37006eaf9d91ace7d922
Cam <cameronmcouch@gmail.com>
2021-05-25 08:13:28 -0400

    testing touch event

--- script.js
+++ script.js
@@ -50,6 +50,7 @@ const gridLi = document.querySelectorAll("li[class*='nav-li']");
  */
 const updateView = function (evt) {
     const evtType = evt.type;
+    if(evtType === 'touchstart') { let p = document.createElement('p'); p.innerText = evtType; document.appendChild(p); }
     const key = evt.type === 'keydown' ? evt.code : '';
     if (!key && typeof this != undefined && this.value.toString() === sectionWrapper.lastClicked) {
         return;
