27625476dba5fe009620a14a565dc39cea407920
Cam <cameronmcouch@gmail.com>
2021-05-28 09:02:28 -0400

    added cool homegrown slidy gradient clip

--- script.js
+++ script.js
@@ -2,21 +2,6 @@
 const footer = document.getElementsByTagName('footer')[0];
 const header = document.getElementsByTagName('header')[0];
 
-const smallScreen = function () {
-    if (window.innerHeight <= 850 && window.innerWidth <= 450) {
-        if (footer.childElementCount === 0) {
-            footer.appendChild(document.getElementsByTagName('nav')[0]);
-        }
-    } else {
-        if (header.childElementCount === 0) {
-            header.appendChild(document.getElementsByTagName('nav')[0]);
-        }
-    }
-    window.addEventListener('resize', smallScreen, false);
-}
-
-smallScreen();
-
 /***
  * @object Stores reference to the section parent element, and the last clicked navElement
  */
@@ -24,6 +9,11 @@ let sectionWrapper = {
     parent: document.getElementById('section-wrapper'),
     lastClicked: '0'
 }
+
+// get all section and li elements with these attributes. We will access these in updateView.
+const gridSections = document.querySelectorAll("section[id$='-section']");
+const gridLi = document.querySelectorAll("li[class*='nav-li']");
+const slidingBorder = document.getElementById('border-slide');
 
 /***
  * @constant Zero-indexed items mapped to strings that mimic standard counting. Through 9 / "ten". Eleven would be achieved by concat textEquivalents.9 + textEquivalents.0 
@@ -41,18 +31,28 @@ const textEquivalents = {
     9: "ten"
 }
 
-// get all section and li elements with these attributes. We will access these in updateView.
-const gridSections = document.querySelectorAll("section[id$='-section']");
-const gridLi = document.querySelectorAll("li[class*='nav-li']");
+const smallScreen = function () {
+    if (window.innerHeight <= 850 && window.innerWidth <= 450) {
+        if (footer.childElementCount === 0) {
+            footer.appendChild(document.getElementsByTagName('nav')[0]);
+            footer.appendChild(slidingBorder);
+        }
+    } else {
+        if (header.childElementCount === 0) {
+            header.appendChild(document.getElementsByTagName('nav')[0]);
+            header.appendChild(slidingBorder);
+        }
+    }
+    window.addEventListener('resize', smallScreen, false);
+}
+
+smallScreen();
 
 /***
  * @function On click of a nav element or left/right arrow press, update the view and lastFocused. If the target of the click is the li tied to the current view, do nothing.
  */
 const updateView = function (evt) {
     const evtType = evt.type;
-    let p = document.createElement('p'); 
-    p.innerText = evtType; 
-    document.body.appendChild(p);
     const key = evt.type === 'keydown' ? evt.code : '';
     if (!key && typeof this != undefined && this.value.toString() === sectionWrapper.lastClicked) {
         return;
@@ -66,23 +66,23 @@ const updateView = function (evt) {
 const handleViewChangeEvent = function (data) {
     // click value comes in as a number, parse it as a string
     data = typeof data === 'number' ? data.toString() : data;
-    if(typeof data === 'string' && data.indexOf('Arrow') > -1) {
-        if(sectionWrapper.lastClicked == '0') {
-            if(data === 'ArrowLeft') {
+    if (typeof data === 'string' && data.indexOf('Arrow') > -1) {
+        if (sectionWrapper.lastClicked == '0') {
+            if (data === 'ArrowLeft') {
                 data = '2';
-            } else if(data === 'ArrowRight') {
+            } else if (data === 'ArrowRight') {
                 data = '1';
             }
-        } else if(sectionWrapper.lastClicked == '2') {
-            if(data === 'ArrowLeft') {
+        } else if (sectionWrapper.lastClicked == '2') {
+            if (data === 'ArrowLeft') {
                 data = '1';
-            } else if(data === 'ArrowRight') {
+            } else if (data === 'ArrowRight') {
                 data = '0';
             }
         } else {
-            if(data === 'ArrowLeft') {
+            if (data === 'ArrowLeft') {
                 data = '0';
-            } else if(data === 'ArrowRight') {
+            } else if (data === 'ArrowRight') {
                 data = '2';
             }
         }
@@ -95,8 +95,10 @@ const handleViewChangeEvent = function (data) {
             );
             gridSections[0].classList.replace('hide', 'visible');
             gridLi[0].classList.remove('desaturate');
+            gridLi[0].parentElement.classList.replace('shrink', 'grow');
             gridSections[sectionWrapper.lastClicked].classList.replace('visible', 'hide');
             gridLi[sectionWrapper.lastClicked].classList.add('desaturate');
+            slidingBorder.classList.replace(`behind-${textEquivalents[sectionWrapper.lastClicked]}`, 'behind-one');
             sectionWrapper.lastClicked = data;
             break;
         case '1':
@@ -106,8 +108,10 @@ const handleViewChangeEvent = function (data) {
             );
             gridSections[1].classList.replace('hide', 'visible');
             gridLi[1].classList.remove('desaturate');
+            gridLi[1].parentElement.classList.replace('shrink', 'grow');
             gridSections[sectionWrapper.lastClicked].classList.replace('visible', 'hide');
             gridLi[sectionWrapper.lastClicked].classList.add('desaturate');
+            slidingBorder.classList.replace(`behind-${textEquivalents[sectionWrapper.lastClicked]}`, 'behind-two');
             sectionWrapper.lastClicked = data;
             break;
         case '2':
@@ -117,8 +121,10 @@ const handleViewChangeEvent = function (data) {
             );
             gridSections[2].classList.replace('hide', 'visible');
             gridLi[2].classList.remove('desaturate');
+            gridLi[2].parentElement.classList.replace('shrink', 'grow');
             gridSections[sectionWrapper.lastClicked].classList.replace('visible', 'hide');
             gridLi[sectionWrapper.lastClicked].classList.add('desaturate');
+            slidingBorder.classList.replace(`behind-${textEquivalents[sectionWrapper.lastClicked]}`, 'behind-three');
             sectionWrapper.lastClicked = data;
             break;
     }
@@ -126,7 +132,6 @@ const handleViewChangeEvent = function (data) {
 // attach event listeners to the nav
 gridLi.forEach(item => { item.addEventListener('click', updateView, false); });
 gridLi.forEach(item => { item.addEventListener('touchstart', updateView, false); });
-// swipe event
 // listening for arrow left and right to allow for navigation of the nav elements
 window.addEventListener(
     // property keyCode deprecated due to cross-browser inconsistency -- using code here instead
@@ -135,3 +140,10 @@ window.addEventListener(
     (e) => { (e.code === 'ArrowLeft' || e.code === 'ArrowRight') ? updateView(e) : false },
     false
 );
+window.addEventListener('scroll', () => {
+    let chosenElement;
+    footer.childElementCount > 0 ? (chosenElement = footer, footer.classList.add('scrolled')) : (chosenElement = header, header.classList.add('scrolled'));
+    setTimeout(() => {
+        chosenElement.classList.remove('scrolled');
+    }, 750);
+});
