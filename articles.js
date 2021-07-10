fce58cccc4b533f645becb40bcdce1498c4f4de3
Cam <cameronmcouch@gmail.com>
2021-07-10 08:38:22 -0400

    new content css for layout

--- articles.js
+++ articles.js
@@ -5,11 +5,12 @@ $(function () {
     };
     $.get('https://api.rss2json.com/v1/api.json', data, function (response) {
         if (response.status == 'ok') {
+            console.log(response);
             var output = '<h1>' + "Blog" + '</h1>';
             $.each(response.items, function (k, item) {
                 let date = new Date(item.pubDate);
-                output += '<h2><a href="' + item.link + '" target="_blank" rel="norefferer" title="Will open in a new tab">' + item.title + '</h2></a>';
-                output += '<em><p> Published on ' + date.toDateString() + '</p></em>'
+                output += '<h2>' + item.title + '</h2>';
+                output += '<img id="avatar" src="' + response.feed.image + '">'+'<em><span> Published on ' + date.toDateString() + '</span></em>'
                 output += '<p>' + item.description + '</p>';
                 //add a show more/less button 
                 if(k != response.items.length -1) {
